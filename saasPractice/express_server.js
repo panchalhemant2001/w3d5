598daf653c142a4dbda96d180e7eb2bc5a144307
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-04-27 16:34:45 +0000

    sass syntax practiced

--- saasPractice/express_server.js
+++ saasPractice/express_server.js
@@ -12,7 +12,14 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
+
 app.get("/", (req, res) => {
+  res.redirect("/index");
+});
+
+app.get("/index", (req, res) => {
   res.render("index");
 });
 
